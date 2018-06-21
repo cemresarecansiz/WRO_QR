1a4ef265d60f5d860d749d3c974fb21b60d2ef52
Yiğit Çolakoğlu <yigitcolakoglu@hotmail.com>
2018-06-21 16:41:29 +0300

    fixed

--- Barcode_Reader_Python/run.py
+++
@@ -1,4 +0,0 @@
-import os
-
-script = raw_input("Please enter what you would like to run: ")
-os.system("python " + script + ".py")
---
+++ move_apk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo rm -rf ~/projects/WRO_QR/FoodCloud.apk
+sudo cp '/home/yigido/projects/WRO_QR/FoodCloud/app/build/outputs/apk/debug/app-debug.apk'   ~/projects/WRO_QR/FoodCloud.apk
+sudo chmod 777 ~/projects/WRO_QR/FoodCloud.apk
