793d7304b17b91510d419fb7497decf29060675f
Yiğit Çolakoğlu <yigitcolakoglu@hotmail.com>
2018-06-22 01:09:46 +0300

    connected
    connected all the python files together

--- Barcode_Reader_Python/generate_moves.py
+++ Barcode_Reader_Python/generate_moves.py
@@ -1,35 +1,19 @@
 import sort_date
-exp_date , dates_orig = sort_date.calcDate()
-shelf_back = exp_date[:]
-try:
-    expired = exp_date.count(-1)
-    for i in range(expired):
-        shelf_back.pop(shelf_back.index(-1))
 
-    shelf_new  = sorted(shelf_back)
-    for i in range(expired):
-        shelf_new.append(-1)
-except ValueError:
-    print('')
-exp_date.append(None)
-shelf_new.append(None)
-repeats = 0
 correct_locs = []
 temp = None
 moved_item = None
 moved_item_loc = None
-
-
-print "\n\n\n//////////////////////////////////////////////////////////////////////////////"
-print("                  {}               ".format(exp_date))
-print("                  {}               ".format(shelf_new))
-print "/////////////////////////////////////////////////////////////////////////////\n\n\n"
+exp_date = []
+shelf_new = []
+repeats = 0
 
 def backArray(myArray):
     global shelf_back
     shelf_back = []
     for i in range(len(myArray)):
         shelf_back.append(myArray[i])
+
 
 def eqArray(myArray):
     global exp_date
@@ -45,62 +29,90 @@ def place():
     global shelf_back
     repeats += 1
     backArray(exp_date)
-    
-    if (exp_date[len(exp_date)-1] != None):
+    moved_item = None
+    moved_item_loc = None
+
+    if (exp_date[len(exp_date) - 1] != None):
         empty_loc = exp_date.index(None)
         moved_item_loc = exp_date.index(shelf_new[empty_loc])
-        reps=0
+        reps = 0
         while True:
             reps += 1
-            if(moved_item_loc in correct_locs):
+            if (moved_item_loc in correct_locs):
                 exp_date.pop(moved_item_loc)
                 moved_item_loc = exp_date.index(shelf_new[empty_loc]) + reps
                 continue
             break
         eqArray(shelf_back)
         moved_item = exp_date[moved_item_loc]
-        if(exp_date[moved_item_loc] != shelf_new[moved_item_loc]):
+        if (exp_date[moved_item_loc] != shelf_new[moved_item_loc]):
             exp_date[moved_item_loc] = None
             exp_date[empty_loc] = moved_item
             correct_locs.append(empty_loc)
-            
 
-            print "        Moved item {0} to location {1}. Location {2} is now empty!".format(moved_item,empty_loc + 1, moved_item_loc + 1)
+            print "        Moved item {0} to location {1}. Location {2} is now empty!".format(moved_item, empty_loc + 1,
+                                                                                              moved_item_loc + 1)
             print "============================================================================"
         else:
-            print "      Leaving item {0} in location {1}. Location {2} is still empty!".format(moved_item, moved_item_loc + 1,empty_loc + 1)
+            print "      Leaving item {0} in location {1}. Location {2} is still empty!".format(moved_item,
+                                                                                                moved_item_loc + 1,
+                                                                                                empty_loc + 1)
             print "============================================================================"
             correct_locs.append(moved_item_loc)
         place()
     else:
         return
-    
 
 
+def generate(dates,date_cur):
+    global exp_date
+    global shelf_new
+    global repeats
+    exp_date = sort_date. calcDate(dates,date_cur)
+    shelf_back = exp_date[:]
+    try:
+        expired = exp_date.count(-1)
+        for i in range(expired):
+            shelf_back.pop(shelf_back.index(-1))
 
-while True:
-    if(repeats < len(exp_date)):
-        for i in range(len(exp_date)):
-            if(exp_date[len(exp_date)-1] == None):
-                if(exp_date[i] == shelf_new[i]):
-                    continue
-                else:
-                    exp_date[len(exp_date)-1] = exp_date[i]
-                    print '\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-                    print '     TEMP IS EMPTY, MOVING {0} FROM LOCATION {1} TO THE TEMP'.format(exp_date[i],i+1)
-                    print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n'
-                    print "============================================================================"
-                    exp_date[i] = None
+        shelf_new  = sorted(shelf_back)
+        for i in range(expired):
+            shelf_new.append(-1)
+    except ValueError:
+        print('')
+    exp_date.append(None)
+    shelf_new.append(None)
+    repeats = 0
 
 
-    else:
-        break
-    place()
+    print "\n\n\n//////////////////////////////////////////////////////////////////////////////"
+    print("                  {}               ".format(exp_date))
+    print("                  {}               ".format(shelf_new))
+    print "/////////////////////////////////////////////////////////////////////////////\n\n\n"
 
-try:
-    print('***********************************************************************************')
-    print('          The last {} items have expired, throwing them away.'.format(len(exp_date)-shelf_new.index(-1)-1))
-    print('***********************************************************************************')
-except ValueError:
-    print("                              No items have expired")
-    print('***********************************************************************************')
+    while True:
+        if(repeats < len(exp_date)):
+            for i in range(len(exp_date)):
+                if(exp_date[len(exp_date)-1] == None):
+                    if(exp_date[i] == shelf_new[i]):
+                        continue
+                    else:
+                        exp_date[len(exp_date)-1] = exp_date[i]
+                        print '\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+                        print '     TEMP IS EMPTY, MOVING {0} FROM LOCATION {1} TO THE TEMP'.format(exp_date[i],i+1)
+                        print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n'
+                        print "============================================================================"
+                        exp_date[i] = None
+
+
+        else:
+            break
+        place()
+
+    try:
+        print('***********************************************************************************')
+        print('          The last {} items have expired, throwing them away.'.format(len(exp_date)-shelf_new.index(-1)-1))
+        print('***********************************************************************************')
+    except ValueError:
+        print("                              No items have expired")
+        print('***********************************************************************************')
--- Barcode_Reader_Python/main.py
+++ Barcode_Reader_Python/main.py
@@ -5,10 +5,23 @@ import imutils
 import time
 import cv2
 import database_actions
-
+import datetime
+import generate_moves
 
 # initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream...")
+def dateStr():
+    date_cur = ""
+    if len(str(datetime.date.today().day)) == 1:
+        date_cur = date_cur + "0" + str(datetime.date.today().day) + "."
+    else:
+        date_cur = date_cur + str(datetime.date.today().day) + "."
+    if len(str(datetime.date.today().month)) == 1:
+        date_cur = date_cur + "0" + str(datetime.date.today().month) + "."
+    else:
+        date_cur = date_cur + str(datetime.date.today().month) + "."
+    date_cur += str(datetime.date.today().year)
+    return date_cur
 # vs = VideoStream(src=0).start()
 vs = VideoStream().start()
 time.sleep(2.0)
@@ -17,8 +30,9 @@ reps = 0
 barcodes = None
 prevcode = None
 exp_date = []
+current_date =   dateStr()
 
-
+print current_date
 # loop over the frames from the video stream
 while reps<1:
     # grab the frame from the threaded video stream and resize it to
@@ -58,5 +72,7 @@ while reps<1:
 # close the output CSV file do a bit of cleanup
 print("[INFO] cleaning up...")
 print dates
+generate_moves.generate(dates,current_date)
+
 cv2.destroyAllWindows()
 vs.stop()
